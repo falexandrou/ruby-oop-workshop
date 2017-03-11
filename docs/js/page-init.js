d3bacd3bbc3bda0499c07004a3d70baa6e72d8a2
Fotis Alexandrou <falexandrou@gmail.com>
2017-03-11 20:46:22 +0200

    clean up the things

--- docs/js/page-init.js
+++ docs/js/page-init.js
@@ -8,29 +8,7 @@ var W4C = window.W4C || {};
 
 
 	var init = function () {
-		$(function () {
-			// p2pu tab
-			$(".p2pu-tab").p2puSlider({
-				navbarContainer: '.navbar',
-				icon: '.p2pu-tab-icon',
-				iconUp: 'fa fa-chevron-down',
-				iconDown: 'fa fa-chevron-up'
-			});
-
-			// Smoth scrolling
-			$('a[href*=#]:not([href=#])').click(function () {
-				if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-					if (target.length) {
-						$('html,body').animate({
-							scrollTop: target.offset().top - 100
-						}, 1000);
-						return false;
-					}
-				}
-			});
-		});
+		$(function () {});
 	};
 
 	W4C.Splash = {};
