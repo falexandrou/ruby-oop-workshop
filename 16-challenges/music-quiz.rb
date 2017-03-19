daa0d2b109c12766f3269102fb0c3be9a8185deb
Fotis Alexandrou <falexandrou@gmail.com>
2017-03-19 22:14:23 +0200

    replace blog with slides

---
+++ 16-challenges/music-quiz.rb
@@ -0,0 +1,21 @@
+# Create a music trivia quiz that tests the knowledge on your favorite bands / musicians
+# Hint: You need to have an array of questions and a multiple choice prompt
+
+class MusicQuiz
+  def initialize
+    @answers = []
+  end
+
+  def questions
+  end
+
+  def answers
+  end
+
+  def ask_question
+  end
+
+  def check_answer
+  end
+
+end
